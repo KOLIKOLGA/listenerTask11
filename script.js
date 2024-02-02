90d7c737445d9c6b16f17af2c1b2783cdc8b99c4
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2024-02-02 08:18:12 +0300

    getImputColor, range.addEventListener(input)

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
+const btn = document.getElementById("btn");
+const square = document.getElementById("square");
+const inputText = document.getElementById("text");
+const span = document.getElementById("text-span");
+const range = document.getElementById("range");
+const spanRange = document.getElementById("range-span");
+const link = document.querySelector("a");
+const btnE = document.querySelector("#e_btn");
+const circle = document.querySelector("#circle");
 
+btnE.style = "display: none; ";
 
+let color;
+const getInputColor = function () {
+  color = inputText.value;
+  square.style.backgroundColor = ` ${color}`;
+};
+btn.addEventListener("click", getInputColor);
 
+range.addEventListener("input", function (event) {
+  spanRange.textContent = event.target.value;
 
+  circle.style.width = event.target.value + "%";
+  circle.style.height = event.target.value + "%";
+});
 
+// text.addEventListener("change", getInputColor);
+
+// console.log(btn);
+// console.log(btnE);
+// console.log(square);
+// console.log(inputText);
+// console.log(span);
+// console.log(spanRange);
+// console.log(circle);
+// console.log(range);
+// console.log(link);
